5dee00afe25ebfb22c124d56b81ae08aae33dd1d
Myproject <mohamedAlaa-CS>
2024-02-01 21:48:32 +0200

    add app color

--- lib/core/helper/utilities/app_color.dart
+++ lib/core/helper/utilities/app_color.dart
@@ -9,21 +9,10 @@ class AppColors {
 
   // Semantic colors
 
-  static const Color main = Color(0xFF2C6AA5);
-  static const Color laightMain = Color(0xFFF2F9FF);
-  static const Color laight3Main = Color(0xFFEBF6FF);
-  static const Color grey1 = Color.fromARGB(255, 223, 220, 220);
-  static const Color grey2 = Color(0xFFF6F6F6);
-  static const Color grey3 = Color(0xFFF9F9F9);
-  static const Color laight2Main = Color(0xFF579FE3);
-  static const Color success = Color(0xFF95C11F);
-  static const Color warning = Color(0xFFFCAC4E);
-  static const Color danger = Color(0xFFD03D78);
-  static const Color boldDanger = Color(0xFFB52A71);
-  static const Color lightDanger = Color(0xFFFFE1ED);
-  static const Color darkgrey = Color(0xFF515966);
-  static const Color text = Color(0xFF23476A);
-  static const Color scafold = Color(0xFFFCFCFC);
+  static const Color main = Color(0xFF004182);
+  static const Color blueLight = Color(0xff0041824d);
+  static const Color textColor = Color(0xff06004F);
+  static const Color descriptionColor = Color(0xff06004f99);
 
   // Button colors
 }
