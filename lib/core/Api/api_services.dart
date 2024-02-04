1f7feda7b1bb9275022631a49c4528e69a3cbb00
Myproject <mohamedAlaa-CS>
2024-02-04 20:17:21 +0200

    api service

--- lib/core/Api/api_services.dart
+++ lib/core/Api/api_services.dart
@@ -1,1 +1,30 @@
-class ApiServices {}
+import 'package:dio/dio.dart';
+import 'package:e_commerce/injections.dart';
+
+class ApiServices {
+  static Future<Map<String, dynamic>> get({
+    required String endPoint,
+    Map<String, dynamic>? data,
+    Map<String, dynamic>? query,
+  }) async {
+    Response response = await getIt.get<Dio>().get(
+          endPoint,
+          data: data,
+          queryParameters: query,
+        );
+    return response.data;
+  }
+
+  static Future<Map<String, dynamic>> post({
+    required String endPoint,
+    Map<String, dynamic>? data,
+    Map<String, dynamic>? query,
+  }) async {
+    Response response = await getIt.get<Dio>().get(
+          endPoint,
+          data: data,
+          queryParameters: query,
+        );
+    return response.data;
+  }
+}
