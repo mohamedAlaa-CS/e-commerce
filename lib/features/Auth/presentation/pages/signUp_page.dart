4c7d99b58fe1adc88aa6d05742d463f9140a0047
Myproject <mohamedAlaa-CS>
2024-02-05 17:47:20 +0200

    create signp page

---
+++ lib/features/Auth/presentation/pages/signUp_page.dart
@@ -0,0 +1,21 @@
+import 'package:e_commerce/core/helper/extensions/assetss_widgets.dart';
+import 'package:e_commerce/core/helper/utilities/app_color.dart';
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.main,
+      body: SafeArea(
+          child: Padding(
+        padding: 14.aEdge,
+        child: const Column(
+          children: [],
+        ),
+      )),
+    );
+  }
+}
