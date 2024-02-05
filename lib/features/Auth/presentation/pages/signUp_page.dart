54f9c5555c36538096eb93ba840a45fa1b0fee9a
Myproject <mohamedAlaa-CS>
2024-02-05 18:23:36 +0200

    triggle cubit in signup page

--- lib/features/Auth/presentation/pages/signUp_page.dart
+++ lib/features/Auth/presentation/pages/signUp_page.dart
@@ -2,9 +2,13 @@ import 'package:e_commerce/core/helper/extensions/assetss_widgets.dart';
 import 'package:e_commerce/core/helper/extensions/context_size.dart';
 import 'package:e_commerce/core/helper/utilities/app_color.dart';
 import 'package:e_commerce/core/helper/utilities/app_strings.dart';
+import 'package:e_commerce/core/helper/utilities/app_validator.dart';
 import 'package:e_commerce/core/widgets/app_text_form.dart';
 import 'package:e_commerce/core/widgets/main_buttom.dart';
+import 'package:e_commerce/features/Auth/data/repos/auth_repo_impel.dart';
+import 'package:e_commerce/features/Auth/presentation/manager/signup_cubit/signup_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 
 class SignupPage extends StatelessWidget {
@@ -17,51 +21,73 @@ class SignupPage extends StatelessWidget {
       body: SafeArea(
           child: Padding(
         padding: 14.aEdge,
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(height: context.height / 12),
-              Align(
-                alignment: Alignment.center,
-                child: SvgPicture.asset(
-                  AppStrings.logo,
-                  height: 100,
-                  color: AppColors.white,
+        child: BlocProvider(
+          create: (context) => SignupCubit(AuthRepoImpel()),
+          child: BlocConsumer<SignupCubit, SignupState>(
+            listener: (context, state) {},
+            builder: (context, state) {
+              var cubit = SignupCubit.get(context);
+              return SingleChildScrollView(
+                child: Form(
+                  key: cubit.formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(height: context.height / 12),
+                      Align(
+                        alignment: Alignment.center,
+                        child: SvgPicture.asset(
+                          AppStrings.logo,
+                          height: 100,
+                          color: AppColors.white,
+                        ),
+                      ),
+                      30.hSize,
+                      MainTextField(
+                        controller: cubit.name,
+                        validator: AppValidator.nameValidate,
+                        title: 'Full Name',
+                        hintText: 'enter your full name',
+                      ),
+                      18.hSize,
+                      MainTextField(
+                        controller: cubit.phone,
+                        validator: AppValidator.phoneValidate,
+                        title: 'Mobile Number',
+                        hintText: 'enter your mobile no.',
+                      ),
+                      18.hSize,
+                      MainTextField(
+                        controller: cubit.email,
+                        validator: AppValidator.emailValidate,
+                        title: 'E-mail address',
+                        hintText: 'enter your email address',
+                      ),
+                      18.hSize,
+                      MainTextField(
+                        controller: cubit.poassWord,
+                        validator: AppValidator.passwordValidate,
+                        title: 'Password',
+                        hintText: 'enter your password',
+                      ),
+                      18.hSize,
+                      MainTextField(
+                        controller: cubit.rePoassWord,
+                        validator: AppValidator.passwordValidate,
+                        title: ' confirm Password',
+                        hintText: 'enter your password',
+                      ),
+                      24.hSize,
+                      MainButtom(
+                        text: 'Sign up',
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              30.hSize,
-              const MainTextField(
-                title: 'Full Name',
-                hintText: 'enter your full name',
-              ),
-              18.hSize,
-              const MainTextField(
-                title: 'Mobile Number',
-                hintText: 'enter your mobile no.',
-              ),
-              18.hSize,
-              const MainTextField(
-                title: 'E-mail address',
-                hintText: 'enter your email address',
-              ),
-              18.hSize,
-              const MainTextField(
-                title: 'Password',
-                hintText: 'enter your password',
-              ),
-              18.hSize,
-              const MainTextField(
-                title: ' confirm Password',
-                hintText: 'enter your password',
-              ),
-              24.hSize,
-              MainButtom(
-                text: 'Sign up',
-                onPressed: () {},
-              ),
-            ],
+              );
+            },
           ),
         ),
       )),
