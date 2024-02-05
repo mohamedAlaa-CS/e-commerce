24aaca2ed13cfb46359911b7f1e648b9a295ca54
Myproject <mohamedAlaa-CS>
2024-02-05 17:55:24 +0200

    finsh signup ui

--- lib/features/Auth/presentation/pages/signUp_page.dart
+++ lib/features/Auth/presentation/pages/signUp_page.dart
@@ -1,6 +1,11 @@
 import 'package:e_commerce/core/helper/extensions/assetss_widgets.dart';
+import 'package:e_commerce/core/helper/extensions/context_size.dart';
 import 'package:e_commerce/core/helper/utilities/app_color.dart';
+import 'package:e_commerce/core/helper/utilities/app_strings.dart';
+import 'package:e_commerce/core/widgets/app_text_form.dart';
+import 'package:e_commerce/core/widgets/main_buttom.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SignupPage extends StatelessWidget {
   const SignupPage({super.key});
@@ -12,8 +17,52 @@ class SignupPage extends StatelessWidget {
       body: SafeArea(
           child: Padding(
         padding: 14.aEdge,
-        child: const Column(
-          children: [],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: context.height / 12),
+              Align(
+                alignment: Alignment.center,
+                child: SvgPicture.asset(
+                  AppStrings.logo,
+                  height: 100,
+                  color: AppColors.white,
+                ),
+              ),
+              30.hSize,
+              const MainTextField(
+                title: 'Full Name',
+                hintText: 'enter your full name',
+              ),
+              18.hSize,
+              const MainTextField(
+                title: 'Mobile Number',
+                hintText: 'enter your mobile no.',
+              ),
+              18.hSize,
+              const MainTextField(
+                title: 'E-mail address',
+                hintText: 'enter your email address',
+              ),
+              18.hSize,
+              const MainTextField(
+                title: 'Password',
+                hintText: 'enter your password',
+              ),
+              18.hSize,
+              const MainTextField(
+                title: ' confirm Password',
+                hintText: 'enter your password',
+              ),
+              24.hSize,
+              MainButtom(
+                text: 'Sign up',
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
       )),
     );
