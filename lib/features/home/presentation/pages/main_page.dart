e97b31a2819e46c94cf9ea49778c5c50b74e4778
Myproject <mohamedAlaa-CS>
2024-02-07 17:27:41 +0200

    create all page to bottom nav bar

--- lib/features/home/presentation/pages/main_page.dart
+++ lib/features/home/presentation/pages/main_page.dart
@@ -1,6 +1,10 @@
 import 'package:e_commerce/core/helper/utilities/app_color.dart';
 import 'package:e_commerce/core/helper/utilities/app_strings.dart';
+import 'package:e_commerce/features/categores/persentation/pages/category_page.dart';
+import 'package:e_commerce/features/home/presentation/pages/home_page.dart';
 import 'package:e_commerce/features/home/presentation/widgets/bottom_nav_item.dart';
+import 'package:e_commerce/features/loves/persentation/pages/loves_page.dart';
+import 'package:e_commerce/features/person/persentation/pages/person_page.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -12,40 +16,44 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   int currentIndex = 0;
-
+  List<Widget> screenList = [
+    const HomePage(),
+    const CategoryPage(),
+    const LovePage(),
+    const PersonPage()
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(bottomNavigationBar: customBottomNavigationBar());
-  }
-
-  Widget customBottomNavigationBar() {
-    return ClipRRect(
-      borderRadius: const BorderRadius.only(
-        topRight: Radius.circular(30),
-        topLeft: Radius.circular(30),
+    return Scaffold(
+      body: screenList[currentIndex],
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topRight: Radius.circular(30),
+          topLeft: Radius.circular(30),
+        ),
+        child: BottomNavigationBar(
+            currentIndex: currentIndex,
+            onTap: (value) {
+              currentIndex = value;
+              setState(() {});
+            },
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            backgroundColor: AppColors.main,
+            type: BottomNavigationBarType.fixed,
+            selectedFontSize: 0,
+            unselectedFontSize: 0,
+            items: [
+              bootomNAvItem(
+                  image: AppStrings.homeIcon, select: currentIndex == 0),
+              bootomNAvItem(
+                  image: AppStrings.categoryIcon, select: currentIndex == 1),
+              bootomNAvItem(
+                  image: AppStrings.loveIcon, select: currentIndex == 2),
+              bootomNAvItem(
+                  image: AppStrings.personIcon, select: currentIndex == 3),
+            ]),
       ),
-      child: BottomNavigationBar(
-          currentIndex: currentIndex,
-          onTap: (value) {
-            currentIndex = value;
-            setState(() {});
-          },
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          backgroundColor: AppColors.main,
-          type: BottomNavigationBarType.fixed,
-          selectedFontSize: 0,
-          unselectedFontSize: 0,
-          items: [
-            bootomNAvItem(
-                image: AppStrings.homeIcon, select: currentIndex == 0),
-            bootomNAvItem(
-                image: AppStrings.categoryIcon, select: currentIndex == 1),
-            bootomNAvItem(
-                image: AppStrings.loveIcon, select: currentIndex == 2),
-            bootomNAvItem(
-                image: AppStrings.personIcon, select: currentIndex == 3),
-          ]),
     );
   }
 }
---
+++ lib/features/person/persentation/pages/person_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PersonPage extends StatelessWidget {
+  const PersonPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'person',
+        // style: TextStyle(color: Colors.black),
+      ),
+    );
+  }
+}
