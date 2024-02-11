5b8c5b48d4efc02579a5cb2d5bdd0bd78b053409
Myproject <mohamedAlaa-CS>
2024-02-11 16:14:04 +0200

    home repo impel

---
+++ lib/features/home/data/repos/Home_repo_impel.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:e_commerce/core/errors/failuer.dart';
+import 'package:e_commerce/features/home/data/data_source/home_local_data_source.dart';
+import 'package:e_commerce/features/home/data/data_source/home_remote_data_source.dart';
+import 'package:e_commerce/features/home/domain/entity/categort_entity.dart';
+import 'package:e_commerce/features/home/domain/repos/home_repo.dart';
+
+class HomeRepoImpel implements HomeRepo {
+  final HomeLocalDataSorce homeLocalDataSorce;
+  final HomeRemoteDataSource homeRemoteDataSource;
+
+  HomeRepoImpel({
+    required this.homeLocalDataSorce,
+    required this.homeRemoteDataSource,
+  });
+
+  @override
+  Future<Either<Failure, List<CategoryEntity>>> getCategories() async {
+    try {
+      var data = await homeRemoteDataSource.getCAtegories();
+      return right(data);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDiorError(e));
+      } else {
+        return left(ServerFailure(e.toString()));
+      }
+    }
+  }
+}
