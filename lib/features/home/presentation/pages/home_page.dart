a243630a56e9c3c27b740a742240fc06c0845c5a
Myproject <mohamedAlaa-CS>
2024-02-11 14:39:40 +0200

    home appliance list view item

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:e_commerce/core/helper/utilities/app_strings.dart';
 import 'package:e_commerce/core/widgets/app_text_form.dart';
 import 'package:e_commerce/core/widgets/main_text.dart';
 import 'package:e_commerce/features/home/presentation/pages/widgets/category_grid_view_item.dart';
+import 'package:e_commerce/features/home/presentation/pages/widgets/home_appliance_list_view_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -15,76 +16,90 @@ class HomePage extends StatelessWidget {
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.only(left: 14),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SvgPicture.asset(
-              AppStrings.logo,
-              height: 30,
-              color: AppColors.main,
-            ),
-            20.hSize,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: MainTextField(
-                    hintText: 'what do you search for?',
-                    prefixIcon: Icons.search,
-                    fillColor: Colors.transparent,
-                    border: searchBorder(),
-                    focusedBorder: searchBorder(),
-                    enabledBorder: searchBorder(),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SvgPicture.asset(
+                AppStrings.logo,
+                height: 30,
+                color: AppColors.main,
+              ),
+              20.hSize,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: MainTextField(
+                      hintText: 'what do you search for?',
+                      prefixIcon: Icons.search,
+                      fillColor: Colors.transparent,
+                      border: searchBorder(),
+                      focusedBorder: searchBorder(),
+                      enabledBorder: searchBorder(),
+                    ),
                   ),
-                ),
-                20.wSize,
-                InkWell(
-                  onTap: () {},
-                  child: SvgPicture.asset(
-                    AppStrings.cartIcon,
-                    height: 30,
+                  20.wSize,
+                  InkWell(
+                    onTap: () {},
+                    child: SvgPicture.asset(
+                      AppStrings.cartIcon,
+                      height: 30,
+                    ),
                   ),
-                ),
-                20.wSize
-              ],
-            ),
-            15.hSize,
-            Image.asset(AppStrings.baner_1),
-            10.hSize,
-            const Row(
-              children: [
-                MainText.heading('Categories'),
-                Spacer(),
-                MainText.body('view all')
-              ],
-            ),
-            10.hSize,
-            SizedBox(
-              height: 136,
-              child: ListView.separated(
-                  scrollDirection: Axis.horizontal,
-                  separatorBuilder: (context, index) => 8.wSize,
-                  itemCount: 10,
-                  itemBuilder: (context, index) => const CategoryGrideViewItem(
-                        image:
-                            'https://fanbooster.com/wp-content/uploads/2020/02/SocialMediaImageSizes2020.jpg',
-                        text: 'Headphones ',
-                      )),
-            ),
-            SizedBox(
-              height: 130,
-              child: ListView.separated(
+                  20.wSize
+                ],
+              ),
+              15.hSize,
+              Image.asset(AppStrings.baner_1),
+              10.hSize,
+              Row(
+                children: [
+                  const MainText.heading('Categories'),
+                  const Spacer(),
+                  const MainText.body('view all'),
+                  10.wSize,
+                ],
+              ),
+              10.hSize,
+              SizedBox(
+                height: 136,
+                child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    separatorBuilder: (context, index) => 8.wSize,
+                    itemCount: 10,
+                    itemBuilder: (context, index) =>
+                        const CategoryGrideViewItem(
+                          image:
+                              'https://fanbooster.com/wp-content/uploads/2020/02/SocialMediaImageSizes2020.jpg',
+                          text: 'Headphones ',
+                        )),
+              ),
+              SizedBox(
+                height: 130,
+                child: ListView.separated(
                   padding: EdgeInsets.zero,
                   scrollDirection: Axis.horizontal,
                   separatorBuilder: (context, index) => 8.wSize,
                   itemCount: 10,
                   itemBuilder: (context, index) => const CategoryGrideViewItem(
-                        image:
-                            'https://fanbooster.com/wp-content/uploads/2020/02/SocialMediaImageSizes2020.jpg',
-                        text: 'Headphones ',
-                      )),
-            ),
-          ],
+                    image:
+                        'https://fanbooster.com/wp-content/uploads/2020/02/SocialMediaImageSizes2020.jpg',
+                    text: 'Headphones ',
+                  ),
+                ),
+              ),
+              const MainText.heading('Home Appliance'),
+              5.hSize,
+              HomeAppllianceListViewItem(
+                image:
+                    'https://fanbooster.com/wp-content/uploads/2020/02/SocialMediaImageSizes2020.jpg',
+                price: 200.toString(),
+                review: 4.5.toString(),
+                title: 'Headphones',
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/features/home/presentation/pages/widgets/home_appliance_list_view_item.dart
@@ -0,0 +1,101 @@
+import 'package:e_commerce/core/helper/extensions/assetss_widgets.dart';
+import 'package:e_commerce/core/helper/utilities/app_color.dart';
+import 'package:e_commerce/core/helper/utilities/app_strings.dart';
+import 'package:e_commerce/core/widgets/main_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HomeAppllianceListViewItem extends StatelessWidget {
+  const HomeAppllianceListViewItem(
+      {super.key,
+      required this.image,
+      required this.title,
+      required this.review,
+      required this.price});
+  final String image;
+  final String title;
+  final String review;
+  final String price;
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: 10.cBorder,
+      child: Container(
+        width: 165,
+        decoration: const BoxDecoration(
+          color: AppColors.white,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                SizedBox(
+                  height: 100,
+                  width: double.infinity,
+                  child: Image.network(
+                    image,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  right: 8,
+                  top: 8,
+                  child: Container(
+                    padding: 5.aEdge,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: AppColors.white,
+                    ),
+                    child: SvgPicture.asset(
+                      AppStrings.loveIcon,
+                      color: AppColors.main,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            5.hSize,
+            Container(
+              padding: 10.aEdge,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  MainText.heading(title),
+                  Row(
+                    children: [
+                      MainText.body('Review ($review)'),
+                      20.wSize,
+                      const Icon(
+                        Icons.star,
+                        color: Color(0xffFDD835),
+                      )
+                    ],
+                  ),
+                  8.hSize,
+                  Row(
+                    children: [
+                      MainText.body('EGP $price'),
+                      const Spacer(),
+                      Container(
+                        padding: 6.aEdge,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: AppColors.main,
+                        ),
+                        child: const Icon(
+                          Icons.add,
+                          color: AppColors.white,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
